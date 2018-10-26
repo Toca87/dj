c171ef7ff58a0ed0a36b60948cc1758715c2d340
AndreasBBS <andreas.bbs@gmail.com>
2018-10-26 21:41:14 +0100

    Updates

---
+++ precache-manifest.353db1ccd88f71f4c4ae2e5ebedd53ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "76b33ae5ac9b1a39d887",
+    "url": "/dj/static/js/runtime~main.76b33ae5.js"
+  },
+  {
+    "revision": "02e50e1f4939f3043648",
+    "url": "/dj/static/js/main.02e50e1f.chunk.js"
+  },
+  {
+    "revision": "077204c4b10c1943ad93",
+    "url": "/dj/static/js/1.077204c4.chunk.js"
+  },
+  {
+    "revision": "02e50e1f4939f3043648",
+    "url": "/dj/static/css/main.9dddd3e1.chunk.css"
+  },
+  {
+    "revision": "077204c4b10c1943ad93",
+    "url": "/dj/static/css/1.03b5cb84.chunk.css"
+  },
+  {
+    "revision": "e5c59e3679d22807e86de0e62a76fcbe",
+    "url": "/dj/index.html"
+  }
+];
